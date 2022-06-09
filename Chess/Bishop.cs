25e3d598e278dc625c7abca899d824741ee79fb4
Marion Gayon <mariongayon@gmail.com>
2022-06-09 14:42:38 +0200

    change capabilities for bishop

--- Chess/Bishop.cs
+++ Chess/Bishop.cs
@@ -1,8 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace Chess
 {
-	public Class1()
-	{
-	}
+    public class Bishop : IPiece
+    {
+        IPiece.Color color;
+        public Bishop(IPiece.Color color)
+        {
+            this.color = color;
+        }
+
+        public bool CanCastle()
+        {
+            return false;
+        }
+
+        public bool CanSkip()
+        {
+            return false;
+        }
+
+        public List<ProspectiveMove> Capabilities()
+        {
+            List<ProspectiveMove> list = new List<ProspectiveMove>();
+            for (int diag = 1; diag <= 7; diag++)
+            {
+                list.Add(new ProspectiveMove(diag, diag));
+                list.Add(new ProspectiveMove(-diag, -diag));
+                list.Add(new ProspectiveMove(-diag, diag));
+                list.Add(new ProspectiveMove(diag, -diag));
+            }
+            return list;
+        }
+
+        public IPiece.Color GetColor()
+        {
+            return color;
+        }
+
+        public bool IsPawn()
+        {
+            return false;
+        }
+    }
 }
+
